dd20ccfc2895df8e139a67be7f4b08d3cdb76db1
LeeSeungGeun <ksks936@gmail.com>
2023-10-04 02:21:28 +0900

    스토리보드 삭제 및 폴더 정리

---
+++ RandomUser/Entity/Request/Page.swift
@@ -0,0 +1,14 @@
+//
+//  Page.swift
+//  RandomUser
+//
+//  Created by 이성근 on 2023/10/04.
+//
+
+import Foundation
+
+struct Page : Codable {
+    let page : Int
+    let results : Int
+    let seed : String
+}
